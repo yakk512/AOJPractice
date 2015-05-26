749dd4a7fe48189bcc4e3943f6d5d03ba419c785
Keita Yamada <yshiro9@yahoo.co.jp>
2015-05-26 22:09:45 +0900

    pass example

--- AreaofPolygons/1100.cpp
+++ AreaofPolygons/1100.cpp
@@ -12,11 +12,14 @@ using namespace::std;
 int main(void)
 {
 
+  int counter = 1;
   int n;
 
-  while(cin >> n , n != 0)
+  while(cin >> n , n != 0 )
     {
       vector< pair<double,double> > vertex(n);
+      double sum = 0.0;
+
       for(int i = 0; i < n; i++)
         {
           cin >> vertex[i].first >> vertex[i].second;
@@ -24,8 +27,19 @@ int main(void)
 
       for(int i = 0; i < vertex.size(); i++)
         {
-          cout << vertex[i].first << "," << vertex[i].second << endl;
+          if( i == vertex.size() - 1 )
+            {
+              sum += 0.5 * ( (vertex[i].first * vertex[0].second) - (vertex[i].second * vertex[0].first) );
+            }
+          else
+            {
+              sum += 0.5 * ( (vertex[i].first * vertex[i+1].second) - (vertex[i].second * vertex[i+1].first) );
+            }
         }
+
+      cout << counter << " " << fabs(sum) << endl;
+      counter++;
+
     }
 
   return 0;
